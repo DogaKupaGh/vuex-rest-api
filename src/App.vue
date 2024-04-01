a145131e8515d9347b3c59404c843c0dc03af89e
dogak <dogakupa@gmail.com>
2024-04-01 16:27:34 +0300

    vuex örnek arşiv

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <div id="app">
+ 
+      <ul>
+       <li v-for="todo in todolarYuklendi" :key="todo.id">{{todo.title}}</li>
+      </ul>
+   </div>
+ </template>
+ 
+ <script>
+ 
+ import {mapActions, mapGetters} from "vuex";
+ export default {
+   name: 'App',
+   methods : {
+     ...mapActions(["todolariGetir"])
+   },
+   computed : {
+     ...mapGetters(["todolarYuklendi"])
+   },
+   created() {
+     this.todolariGetir();
+   }
+ }
+ </script>
+ 
+ <style>
+ #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+   margin-top: 60px;
+ }
+ </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,46 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {createStore} from "vuex";
+
+const store = createStore({
+
+    state(){
+        return {
+            todos : []
+        }
+    },
+    getters : {
+        todolarYuklendi(state)
+        {
+            console.log("todolar yuklendi cagrildi");
+            return state.todos.filter((todo) => todo.completed);
+        }
+    },
+    mutations : {
+        todolariEkle(state,payload) {
+            console.log("todolari ekle cagrildi");
+            state.todos = payload
+        }
+    },
+    actions : {
+        todolariGetir(context)
+        {
+            console.log("todolari getir cagrildi");
+            fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                //console.log(context.c);
+                context.commit("todolariEkle",data);
+            });
+        }
+    }
+ });
+
+const app = createApp(App)
+
+// use the store in the App
+app.use(store)
+
+app.mount('#app')
